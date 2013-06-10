2ccdf301179b49c1df1f3005d0d4b7c995b1861e
Lili Sun <lilis@mit.edu>
2013-06-10 13:34:53 -0400

    everything based in javascript now

--- day1.js
+++ day1.js
@@ -1,7 +1,30 @@
-function calculate(){
-    var input= $('#text1:first');
-    var values=input.val();
-    var out= $('#text1_out:first');
-    out.text(values);
-    console.log(values);
+/*
+    evaluates arithmetic expression in the input field
+    executed when calculate button is clicked
+*/
+function calculate(text){
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g; //this pattern will match numbers, +-*/, and ()
+    var tokens = text.match(pattern); //returns an array
+    return JSON.stringify(tokens); //converts any data structure into string
 }
+
+/*
+    sets up the calculator with input field, calculate button, and output field
+    makes it so that when the button is clicked, output displays what calculate() returns
+*/
+function setup_calc(d){
+    var input = $('<input></input>',{type:"text",size:50});
+    var output = $('<div></div>');
+    var button = $('<button>calculate</button>');
+    button.bind("click",function () {
+        output.html(String(calculate(input.val())));
+    });
+    
+    $(d).append(input,button,output);
+}
+
+$(document).ready(function(){
+    $('.calculator').each(function(){
+        setup_calc(this);
+    });
+});
